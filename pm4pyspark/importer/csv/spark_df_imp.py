da22a4a68d41d9ae0bf703beae3bce6894c8ee03
Jihoon Yang <jihoonyang92@gmail.com>
2019-07-19 03:55:44 +0200

    Importer updated

--- pm4pyspark/importer/csv/spark_df_imp.py
+++ pm4pyspark/importer/csv/spark_df_imp.py
@@ -1,7 +1,27 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+from dateutil import parser, tz
+from pyspark.sql.types import *
 
-def import_sparkdf_from_path(path, sep=None, quote=None, header=None, inferSchema=None):
+def convert_timestamp_to_utc_in_df(df, timest_columns=None):
+    if timest_columns is None:
+        timest_columns = {"time:timestamp"}
+    for col in timest_columns:
+        if df.schema[col].dataType == StringType():
+            utc_zone =  tz.gettz("UTC")
+            func = F.udf(lambda x: parser.parse(x).astimezone(utc_zone).isoformat(), StringType())
+            df = df.withColumn("time_utc", F.to_timestamp(func(df[col]), "yyyy-MM-dd'T'HH:mm:ss"))
+            df = df.drop(col).withColumnRenamed("time_utc", col)
+
+    return df
+
+def import_sparkdf_from_path_wo_timeconversion(path, sep=None, quote=None, header=None, inferSchema=None):
+    spark = SparkSession.builder.getOrCreate()
+    spark_df = spark.read.csv(path, sep=sep, quote=quote, header=header, inferSchema=inferSchema)
+
+    return spark_df
+
+def import_sparkdf_from_path(path, sep=None, quote=None, header=None, inferSchema=None, timest_columns=None, sort=False, sort_field="time:timestamp", ascending=True):
     """
     Imports a Spark Dataframe from the given path of csv format file
     (with selecting the most important columns:
@@ -33,9 +53,14 @@ def import_sparkdf_from_path(path, sep=None, quote=None, header=None, inferSchem
         Spark Dataframe
     """
     spark = SparkSession.builder.getOrCreate()
-    spark_df = spark.read.csv(path, sep=sep, quote=quote, header=header, inferSchema=inferSchema)
-    spark_df = spark_df.select("case:concept:name", "concept:name", F.to_timestamp(spark_df['time:timestamp']).alias('time:timestamp'))\
-        .orderBy("time:timestamp")
+    spark_df = import_sparkdf_from_path_wo_timeconversion(path, sep=sep, quote=quote, header=header, inferSchema=inferSchema)
+    spark_df = convert_timestamp_to_utc_in_df(spark_df, timest_columns=timest_columns)
+
+    if sort and sort_field:
+        if ascending is True:
+            spark_df = spark_df.orderBy("time:timestamp")
+        else:
+            spark_df = spark_df.orderBy("time:timestamp", ascending=False)
 
     return spark_df
 
--- tests/csv_import_test.py
+++ tests/csv_import_test.py
@@ -5,9 +5,22 @@ from pm4pyspark.importer.csv import spark_df_imp as importer
 from pyspark.sql import SparkSession
 import pandas as pd
 
+
+spark_df_wo_timeconversion = importer.import_sparkdf_from_path_wo_timeconversion(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, inferSchema=True)
 spark_df = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, inferSchema=True)
-log = importer.import_log(os.path.join(INPUT_DATA_DIR, "running-example.csv"), parameters={'header': True, 'inferSchema': True})
+spark_df_sorted = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, inferSchema=True, sort=True)
+
+spark_df_wo_timeconversion1 = importer.import_sparkdf_from_path_wo_timeconversion(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, inferSchema=True)
+spark_df1 = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, inferSchema=True)
+spark_df_sorted1 = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, inferSchema=True, sort=True)
+
+spark_df_wo_timeconversion.show(truncate=False)
 spark_df.show()
+spark_df_sorted.show()
+
+spark_df_wo_timeconversion1.show(truncate=False)
+spark_df1.show()
+spark_df_sorted1.show()
 
 '''
 spark = SparkSession.builder.getOrCreate()
