c7c0e2dc30bc9e3cf4d91a5b9534284ac6d1b21c
Jihoon Yang <jihoonyang92@gmail.com>
2019-07-19 04:19:35 +0200

    Duplicates removed

--- pm4pyspark/importer/csv/spark_df_imp.py
+++ pm4pyspark/importer/csv/spark_df_imp.py
@@ -52,7 +52,6 @@ def import_sparkdf_from_path(path, sep=None, quote=None, header=None, inferSchem
     spark_df
         Spark Dataframe
     """
-    spark = SparkSession.builder.getOrCreate()
     spark_df = import_sparkdf_from_path_wo_timeconversion(path, sep=sep, quote=quote, header=header, inferSchema=inferSchema)
     spark_df = convert_timestamp_to_utc_in_df(spark_df, timest_columns=timest_columns)
 
@@ -63,49 +62,3 @@ def import_sparkdf_from_path(path, sep=None, quote=None, header=None, inferSchem
             spark_df = spark_df.orderBy("time:timestamp", ascending=False)
 
     return spark_df
-
-def import_log(path, parameters=None):
-    """
-    Imports a log from the given path of csv format file
-
-    Parameters
-    ----------
-    path:
-        Input CSV file path
-    parameters:
-        sep     ->  column separator in CSV
-        quote   ->  sets a single character used for escaping quoted values where the
-                    separator can be part of the value in CSV. If None is set, it uses the default
-                    value, ``"``. If an empty string is set, it uses ``u0000`` (null character).
-        header  ->  uses the first line as names of columns in CSV. If None is set, it uses the
-                    default value, ``False``.
-        inferSchema ->  infers the input schema automatically from data in CSV. It requires one extra
-                        pass over the data. If None is set, it uses the default value, ``False``.
-
-    Returns
-    -------
-    pair_rdd_group.collect()
-        An event log as a list
-    """
-
-    sep = None
-    quote = None
-    header = False
-
-    if parameters is None:
-        parameters = {}
-    if "sep" in parameters:
-        sep = parameters["sep"]
-    if "quote" in parameters:
-        quote = parameters["quote"]
-    if "header" in parameters:
-        header = parameters["header"]
-    if "inferSchema" in parameters:
-        inferSchema = parameters["inferSchema"]
-
-    spark_df = import_sparkdf_from_path(path, sep=sep, quote=quote, header=header, inferSchema=inferSchema)
-    rdd = spark_df.rdd.map(list)
-    pair_rdd = rdd.map(lambda s: (s[0], (s[1], s[2])))
-    pair_rdd_group = pair_rdd.groupByKey().mapValues(list)
-
-    return pair_rdd_group.collect()
--- tests/csv_import_test.py
+++ tests/csv_import_test.py
@@ -6,13 +6,13 @@ from pyspark.sql import SparkSession
 import pandas as pd
 
 
-spark_df_wo_timeconversion = importer.import_sparkdf_from_path_wo_timeconversion(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, inferSchema=True)
-spark_df = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, inferSchema=True)
-spark_df_sorted = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, inferSchema=True, sort=True)
+spark_df_wo_timeconversion = importer.import_sparkdf_from_path_wo_timeconversion(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True)
+spark_df = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True)
+spark_df_sorted = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"), header=True, sort=True)
 
-spark_df_wo_timeconversion1 = importer.import_sparkdf_from_path_wo_timeconversion(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, inferSchema=True)
-spark_df1 = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, inferSchema=True)
-spark_df_sorted1 = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, inferSchema=True, sort=True)
+spark_df_wo_timeconversion1 = importer.import_sparkdf_from_path_wo_timeconversion(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True)
+spark_df1 = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True)
+spark_df_sorted1 = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True, sort=True)
 
 spark_df_wo_timeconversion.show(truncate=False)
 spark_df.show()
