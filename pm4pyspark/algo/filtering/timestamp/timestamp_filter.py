1fd3850e9e1c3a2530ae9a6b24ef8f190be46ddf
Jihoon Yang <jihoonyang92@gmail.com>
2019-07-21 03:27:11 +0200

    Timestamp Filtering added with the test

---
+++ pm4pyspark/algo/filtering/timestamp/timestamp_filter.py
@@ -0,0 +1,88 @@
+from pm4py.algo.filtering.common.timestamp.timestamp_common import get_dt_from_string
+from pm4py.util.constants import PARAMETER_CONSTANT_TIMESTAMP_KEY, PARAMETER_CONSTANT_CASEID_KEY
+from pm4py.objects.log.util.xes import DEFAULT_TIMESTAMP_KEY
+from pm4py.algo.filtering.common.filtering_constants import CASE_CONCEPT_NAME
+
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
+
+from pm4pyspark.importer.csv import spark_df_imp as importer
+
+
+def filter_traces_contained(df, dt1, dt2, parameters=None):
+    '''
+    Get traces that are contained in the given interval
+    '''
+
+    if parameters is None:
+        parameters = {}
+    timestamp_key = parameters[
+        PARAMETER_CONSTANT_TIMESTAMP_KEY] if PARAMETER_CONSTANT_TIMESTAMP_KEY in parameters else DEFAULT_TIMESTAMP_KEY
+    case_id_glue = parameters[
+        PARAMETER_CONSTANT_CASEID_KEY] if PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+
+    dt1 = get_dt_from_string(dt1)
+    dt2 = get_dt_from_string(dt2)
+    df_converted = importer.convert_timestamp_to_utc_in_df(df)
+    df_ordered = df_converted.orderBy(case_id_glue, timestamp_key)
+    w = Window().partitionBy(case_id_glue).orderBy(timestamp_key)
+    w2 = Window().partitionBy(case_id_glue).orderBy(F.desc(timestamp_key))
+    stacked = df_ordered.withColumn(timestamp_key + "_last", F.max(df_ordered[timestamp_key]).over(w2))
+    stacked = stacked.withColumn(timestamp_key + "_first", F.min(stacked[timestamp_key]).over(w))
+    stacked = stacked.filter(stacked[timestamp_key + "_first"] > dt1)
+    stacked = stacked.filter(stacked[timestamp_key + "_last"] < dt2)
+    stacked_dropped = stacked.drop(timestamp_key + "_last", timestamp_key + "_first")
+
+    return stacked_dropped
+
+def filter_traces_intersecting(df, dt1, dt2, parameters=None):
+    '''
+    Filter traces intersecting the given interval
+    '''
+    df.cache()
+
+    if parameters is None:
+        parameters = {}
+    timestamp_key = parameters[
+        PARAMETER_CONSTANT_TIMESTAMP_KEY] if PARAMETER_CONSTANT_TIMESTAMP_KEY in parameters else DEFAULT_TIMESTAMP_KEY
+    case_id_glue = parameters[
+        PARAMETER_CONSTANT_CASEID_KEY] if PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+
+    dt1 = get_dt_from_string(dt1)
+    dt2 = get_dt_from_string(dt2)
+    df_converted = importer.convert_timestamp_to_utc_in_df(df)
+    df_ordered = df_converted.orderBy(case_id_glue, timestamp_key)
+    w = Window().partitionBy(case_id_glue).orderBy(timestamp_key)
+    w2 = Window().partitionBy(case_id_glue).orderBy(F.desc(timestamp_key))
+    stacked = df_ordered.withColumn(timestamp_key + "_last", F.max(df_ordered[timestamp_key]).over(w2))
+    stacked = stacked.withColumn(timestamp_key + "_first", F.min(stacked[timestamp_key]).over(w))
+    df.unpersist()
+
+    stacked.cache()
+    stacked1 = stacked.filter(stacked[timestamp_key + "_first"] > dt1)
+    stacked1 = stacked1.filter(stacked1[timestamp_key + "_first"] < dt2)
+    stacked2 = stacked.filter(stacked[timestamp_key + "_last"] > dt1)
+    stacked2 = stacked2.filter(stacked2[timestamp_key + "_last"] < dt2)
+    stacked3 = stacked.filter(stacked[timestamp_key + "_first"] < dt1)
+    stacked3 = stacked3.filter(stacked2[timestamp_key + "_last"] > dt2)
+    stacked.unpersist()
+
+    stacked = stacked1.union(stacked2)
+    stacked = stacked.union(stacked3)
+    stacked = stacked.drop(timestamp_key + "_last", timestamp_key + "_first").distinct().orderBy(case_id_glue, timestamp_key)
+
+    return stacked
+
+def apply_events(df, dt1, dt2, parameters=None):
+    if parameters is None:
+        parameters = {}
+    timestamp_key = parameters[
+        PARAMETER_CONSTANT_TIMESTAMP_KEY] if PARAMETER_CONSTANT_TIMESTAMP_KEY in parameters else DEFAULT_TIMESTAMP_KEY
+
+    dt1 = get_dt_from_string(dt1)
+    dt2 = get_dt_from_string(dt2)
+
+    df = df.filter(df[timestamp_key] > dt1)
+    df = df.filter(df[timestamp_key] < dt2)
+
+    return df
---
+++ tests/filtering_timestamp_test.py
@@ -0,0 +1,18 @@
+import os
+import time
+import pandas as pd
+from tests.constants import INPUT_DATA_DIR
+from pm4pyspark.importer.csv import spark_df_imp as importer
+from datetime import datetime
+
+from pm4pyspark.algo.filtering.timestamp import timestamp_filter
+
+spark_df = importer.import_sparkdf_from_path(os.path.join(INPUT_DATA_DIR, "receipt.csv"), header=True)
+spark_df.cache()
+df_timest_contained = timestamp_filter.filter_traces_contained(spark_df, "2011-03-09 00:00:00", "2012-01-18 23:59:59")
+print(df_timest_contained.count())
+print(df_timest_contained.groupby("case:concept:name").count().count())
+
+df_timest_intersecting = timestamp_filter.filter_traces_intersecting(spark_df, "2011-03-09 00:00:00", "2012-01-18 23:59:59")
+print(df_timest_intersecting.count())
+print(df_timest_intersecting.groupby("case:concept:name").count().count())
