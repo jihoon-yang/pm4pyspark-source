8bacf2b6535e86bc10c8f031f27e1d00e0e778f0
Jihoon Yang <jihoonyang92@gmail.com>
2019-08-13 02:09:01 +0200

    Removed duplicates

--- pm4pyspark/exporter/parquet/spark_df_exp.py
+++ pm4pyspark/exporter/parquet/spark_df_exp.py
@@ -9,14 +9,6 @@ from pyspark.sql.types import *
 def export_sparkdf(df, path, case_id_key="case:concept:name", mode=None, partitionBy="@@partitioning", compression=None, num_partitions=128):
 
     get_hash = F.udf(lambda x: abs(hash(x)) % num_partitions, LongType())
-    spark = (SparkSession.
-             builder.
-             master('local[*]').
-             config('spark.executor.memory', '5gb').
-             config("spark.cores.max", "6").
-             getOrCreate())
-
-    #get_hash_udf = F.udf(get_hash, LongType())
 
     df = df.withColumn(partitionBy, get_hash(case_id_key))
     for c in df.columns:
